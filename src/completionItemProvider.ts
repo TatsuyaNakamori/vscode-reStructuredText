2c4061018088d632a38ecb1efb700f8922ecdd42
TatsuyaNakamori <***>
2021-06-18 23:25:06 +0900

    Major updates since the very beginning.

---
+++ src/completionItemProvider.ts
@@ -0,0 +1,202 @@
+// ============================================================
+// Copyright (c) 2021 Tatsuya Nakamori. All rights reserved.
+// See LICENSE in the project root for license information.
+// ============================================================
+import * as vscode from 'vscode';
+import * as util from './util';
+import * as i18n from './i18n';
+
+
+export let headingCompletionTrigger:boolean = false;
+export let listCompletionTrigger:boolean = false;
+export let tableCompletionTrigger:boolean = false;
+
+
+export function setHeadingCompletionTrigger(value:boolean) {
+    headingCompletionTrigger = value;
+}
+
+export function setListCompletionTrigger(value:boolean) {
+    listCompletionTrigger = value;
+}
+
+export function setTableCompletionTrigger(value:boolean) {
+    tableCompletionTrigger = value;
+}
+
+export function heading(
+    document: vscode.TextDocument,
+    position: vscode.Position,
+    context: vscode.CompletionContext):(vscode.CompletionItem[]|undefined) {
+
+    let triggeredChar = context.triggerCharacter;
+    if (!(headingCompletionTrigger || triggeredChar)) {
+        return
+    }
+    // triggeredChar > remapping
+    if (triggeredChar == 'h' || triggeredChar == '1' ) {
+        triggeredChar = '#';
+    } else if (triggeredChar == '2' ) {
+        triggeredChar = '*';
+    } else if (triggeredChar == '3' ) {
+        triggeredChar = '=';
+    } else if (triggeredChar == '4' ) {
+        triggeredChar = '-';
+    } else if (triggeredChar == '5' ) {
+        triggeredChar = '^';
+    } else if (triggeredChar == '6' ) {
+        triggeredChar = '"';
+    }
+
+    const existingText = document.lineAt(position.line).text;
+    if (!existingText.match(/^([-=#^*"]|h[1-6]?)+$/)) {
+        return
+    }
+
+    let completionItemList:vscode.CompletionItem[] = [];
+    const headingInfoList = [
+        ['Heading <h1> [#####]', '##### (<h1>)', '#'],
+        ['Heading <h2> [*****]', '***** (<h2>)', '*'],
+        ['Heading <h3> [=====]', '===== (<h3>)', '='],
+        ['Heading <h4> [-----]', '----- (<h4>)', '-'],
+        ['Heading <h5> [^^^^^]', '^^^^^ (<h5>)', '^'],
+        ['Heading <h6> ["""""]', '""""" (<h6>)', '"']
+    ]
+
+    for (let i = 0; i < headingInfoList.length; i++) {
+        const _headingInfo = headingInfoList[i];
+        const _label       = _headingInfo[0];
+        const _detail      = _headingInfo[1];
+        const _triggerChar = _headingInfo[2];
+
+        const completion = new vscode.CompletionItem(_label);
+        completion.kind = vscode.CompletionItemKind.Module;
+        completion.detail = _detail;
+        completion.documentation =
+            new vscode.MarkdownString(
+                `${i18n.localize("resttext.completion.heading.detail")} \`\`${_detail}\`\``
+            );
+        completion.insertText = "";
+        completion.command = {
+            title: i18n.localize("resttext.heading.add.title"),
+            command: 'resttext.heading.add',
+            arguments: [_triggerChar]
+        };
+
+        if (triggeredChar == _triggerChar || (!triggeredChar && i == 0)) {
+            completion.preselect = true;
+        }
+
+        completionItemList.push(completion);
+    }
+
+    return completionItemList;
+}
+
+export function list(
+    document: vscode.TextDocument,
+    position: vscode.Position,
+    context: vscode.CompletionContext):(vscode.CompletionItem[]|undefined) {
+
+    const triggeredChar = context.triggerCharacter;
+    const tableIsSelected = util.tableIsSelected();
+    if (!(listCompletionTrigger || triggeredChar) || tableIsSelected) {
+        return
+    }
+
+    let completionItemList:vscode.CompletionItem[] = [];
+    const listInfoList = [
+        ["List Numbered [1. ]",   "1. ", "1"],
+        ["List Numbered [#. ]",   "#. ", "#"],
+        ["List Unnumbered [* ]", "* ", "*"]
+    ]
+
+    for (let i = 0; i < listInfoList.length; i++) {
+        const _listInfo = listInfoList[i];
+        const _label       = _listInfo[0];
+        const _detail      = _listInfo[1];
+        const _triggerChar = _listInfo[2];
+
+        const completion = new vscode.CompletionItem(_label);
+        completion.kind = vscode.CompletionItemKind.Module;
+        completion.detail = _detail;
+        completion.sortText = `list_${i}`;
+        completion.documentation =
+            new vscode.MarkdownString(
+                `${i18n.localize("resttext.completion.list.detail")} \`\`${_detail}\`\``
+            );
+        completion.insertText = "";
+
+        completion.command = {
+            title: i18n.localize("resttext.list.add.title"),
+            command: 'resttext.list.add',
+            arguments: [_triggerChar]
+        };
+
+        if (triggeredChar == _triggerChar || (!triggeredChar && i == 0)) {
+            completion.preselect = true;
+        }
+        completionItemList.push(completion);
+    }
+    return completionItemList;
+}
+
+export function table(
+    document: vscode.TextDocument,
+    position: vscode.Position,
+    context: vscode.CompletionContext):(vscode.CompletionItem[]|undefined) {
+
+    const triggeredChar = context.triggerCharacter;
+    if (!(tableCompletionTrigger || triggeredChar)) {
+        return
+    }
+
+    const lineText = document.lineAt(position.line).text;
+    const regRowColumn = /^(?<row>\d+)(?<x>x)(?<column>\d+)$/
+    const match = regRowColumn.exec(lineText);
+    if (!match?.groups) { return }
+
+    const row    = match.groups["row"];
+    const x      = match.groups["x"];
+    const column = match.groups["column"];
+    if (!(row && x && column)) { return }
+
+    let completionItemList:vscode.CompletionItem[] = [];
+    const listInfoList = [
+        [`${row} x ${column} (Table with header)`, `[Header] + [${row} x ${column}]`, "true"],
+        [`${row} x ${column} (Table without header)`, `[${row} x ${column}]`, "false"]
+    ]
+
+    for (let i = 0; i < listInfoList.length; i++) {
+        const _label  = listInfoList[i][0];
+        const _detail = listInfoList[i][0];
+        const _doc    = listInfoList[i][1];
+        if (listInfoList[i][2] == "true") {
+            var _header = true;
+        } else {
+            var _header = false;
+        }
+
+        const completion = new vscode.CompletionItem(_label);
+        completion.kind = vscode.CompletionItemKind.Module;
+        completion.detail = _detail;
+        completion.documentation =
+            new vscode.MarkdownString(
+                `${i18n.localize("resttext.completion.table.detail")} \`\`${_doc}\`\``
+            );
+        completion.insertText = "";
+        completion.command = {
+            title: i18n.localize("resttext.table.create.title"),
+            command: 'resttext.table.createGrid',
+            arguments: [Number(row), Number(column), _header]
+        };
+
+        if (i == 0) {
+            completion.preselect = true;
+        }
+
+        completionItemList.push(completion);
+    }
+
+    return completionItemList;
+}
--- src/extension.ts
+++ src/extension.ts
@@ -3,15 +3,176 @@
 // See LICENSE in the project root for license information.
 // ============================================================
 import * as vscode from 'vscode';
+import * as os from 'os';
+import * as commands from './commands';
+import * as htmlPreview from './htmlPreview';
+import * as listEditor from './listEditor';
+import * as tableEditor from './tableEditor';
+import * as setContext from './setContext';
+import * as completionItemProvider from './completionItemProvider';
+import { SphinxTaskProvider } from './sphinxTaskProvider';
+
+
+let sphinxTaskDisposable: (vscode.Disposable|undefined);
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "resttext" is now active!');
-	let disposable = vscode.commands.registerCommand('resttext.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from reStructuredText!');
-	});
+    console.log('Congratulations, your extension "resttext" is now active!');
 
-	context.subscriptions.push(disposable);
+    // Run it once the first time.
+    setContext.setContext();
+
+    vscode.window.onDidChangeTextEditorSelection((event) => {
+        // console.log(event);
+        setContext.setContext();
+    })
+
+    // ==========================
+    // Register Commands
+    // ==========================
+    // Preview HTML (HTML Build is only supported on Windows.)
+    context.subscriptions.push(
+        vscode.commands.registerCommand('resttext.previewHTMLUserBuild', () => {
+            htmlPreview.showWebView(context, "HTML_USER");
+        })
+    );
+    if (os.platform() == "win32") {
+        context.subscriptions.push(
+            vscode.commands.registerCommand('resttext.previewHTMLHelperBuild', () => {
+                htmlPreview.showWebView(context, "HTML_BUILTIN");
+            })
+        );
+    }
+
+    // Editor
+    context.subscriptions.push(
+        vscode.commands.registerCommand('resttext.test', () => {
+        // test func
+
+    }));
+    context.subscriptions.push(
+        vscode.commands.registerCommand('resttext.bold', () => {
+            commands.bold();
+    }));
+    context.subscriptions.push(
+        vscode.commands.registerCommand('resttext.italic', () => {
+            commands.italic();
+    }));
+    context.subscriptions.push(
+        vscode.commands.registerCommand('resttext.inlineRaw', () => {
+            commands.inlineRaw();
+    }));
+    context.subscriptions.push(
+        vscode.commands.registerCommand('resttext.list', () => {
+            completionItemProvider.setListCompletionTrigger(true);
+            commands.list();
+    }));
+    context.subscriptions.push(
+        vscode.commands.registerCommand('resttext.list.add', (_triggeredChar:string) => {
+            listEditor.addListModifier(_triggeredChar);
+    }));
+    context.subscriptions.push(
+        vscode.commands.registerCommand('resttext.heading', () => {
+            completionItemProvider.setHeadingCompletionTrigger(true);
+            commands.heading();
+    }));
+    context.subscriptions.push(
+        vscode.commands.registerCommand('resttext.heading.add', (_triggeredChar:string) => {
+            commands.addHeading(_triggeredChar);
+    }));
+    context.subscriptions.push(
+        vscode.commands.registerCommand('resttext.table.createGrid', (row:number, column:number, header:boolean) => {
+            const editor = vscode.window.activeTextEditor;
+            if (!editor) { return }
+            const table = new tableEditor.TableEditor(editor);
+            table.createEmptyGrid(row, column, header);
+    }));
+    context.subscriptions.push(
+        vscode.commands.registerCommand('resttext.table.dataToTable', () => {
+            const editor = vscode.window.activeTextEditor;
+            if (!editor) { return }
+            const table = new tableEditor.TableEditor(editor);
+            table.dataToTable();
+    }));
+    context.subscriptions.push(
+        vscode.commands.registerCommand('resttext.key.enter', () => {
+            commands.key_enter();
+    }));
+    context.subscriptions.push(
+        vscode.commands.registerCommand('resttext.key.shift.enter', () => {
+            commands.key_shift_enter();
+    }));
+    context.subscriptions.push(
+        vscode.commands.registerCommand('resttext.key.alt.enter', () => {
+            commands.key_alt_enter();
+    }));
+    context.subscriptions.push(
+        vscode.commands.registerCommand('resttext.key.tab', () => {
+            commands.key_tab();
+    }));
+    context.subscriptions.push(
+        vscode.commands.registerCommand('resttext.key.shift.tab', () => {
+            commands.key_shift_tab();
+    }));
+
+    // ==================================
+    // Register Completion Item Provider
+    // ==================================
+    // Heading
+    context.subscriptions.push(
+        vscode.languages.registerCompletionItemProvider(
+            'restructuredtext',
+            {
+                provideCompletionItems(document, position, token, context) {
+                    const headingCompletionItems =
+                        completionItemProvider.heading(document, position, context);
+                    completionItemProvider.setHeadingCompletionTrigger(false);
+                    return headingCompletionItems;
+                }
+            },
+            ...['#', '*', '=', '-', '^', '"', '1', '2', '3', '4', '5', '6', 'h']
+        )
+    );
+    // List
+    context.subscriptions.push(
+        vscode.languages.registerCompletionItemProvider(
+            'restructuredtext',
+            {
+                provideCompletionItems(document, position, token, context) {
+                    const listCompletionItems =
+                        completionItemProvider.list(document, position, context);
+                    completionItemProvider.setListCompletionTrigger(false);
+                    return listCompletionItems;
+                }
+            },
+            ...[".", "1", "#", "*"]
+        )
+    );
+    // Table
+    context.subscriptions.push(
+        vscode.languages.registerCompletionItemProvider(
+            'restructuredtext',
+            {
+                provideCompletionItems(document, position, token, context) {
+                    const tableCompletionItems =
+                        completionItemProvider.table(document, position, context);
+                    completionItemProvider.setTableCompletionTrigger(false);
+                    return tableCompletionItems;
+                }
+            },
+            ...["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        )
+    );
+
+    // ==================================
+    // Register Task
+    // ==================================
+    sphinxTaskDisposable = vscode.tasks.registerTaskProvider(
+        SphinxTaskProvider.TASK_TYPE, new SphinxTaskProvider()
+    );
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+    if (sphinxTaskDisposable) {
+        sphinxTaskDisposable.dispose();
+    }
+}
